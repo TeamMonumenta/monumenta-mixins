f5008570f889208fbf1cb07e4e860817b2b7721d
Floweynt <guywithanaxe42@gmail.com>
2025-01-17 22:03:01 -0600

    temp

--- src/main/java/com/playmonumenta/papermixins/registry/CommandRegister.java
+++ src/main/java/com/playmonumenta/papermixins/registry/CommandRegister.java
@@ -6,8 +6,8 @@ import com.playmonumenta.papermixins.registry.commands.TellMiniCommand;
 import net.minecraft.commands.CommandSourceStack;
 
 public class CommandRegister {
-    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        TellMiniCommand.register(dispatcher);
-        DebugCommands.register(dispatcher);
-    }
+	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+		TellMiniCommand.register(dispatcher);
+		DebugCommands.register(dispatcher);
+	}
 }
--- src/main/java/com/playmonumenta/papermixins/registry/commands/DebugCommands.java
+++ src/main/java/com/playmonumenta/papermixins/registry/commands/DebugCommands.java
@@ -1,32 +1,33 @@
 package com.playmonumenta.papermixins.registry.commands;
 
+import static com.playmonumenta.papermixins.util.CommandUtil.arg;
+import static com.playmonumenta.papermixins.util.CommandUtil.lit;
+
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.playmonumenta.papermixins.upgradetmp.CommandFunctionDumpUpgrader;
 import com.playmonumenta.papermixins.upgradetmp.CommandJsonDumpUpgrader;
 import com.playmonumenta.papermixins.upgradetmp.CommandSQJsonDumpUpgrader;
 import com.playmonumenta.papermixins.upgradetmp.SimpleCommandUpgrader;
-import static com.playmonumenta.papermixins.util.CommandUtil.arg;
-import static com.playmonumenta.papermixins.util.CommandUtil.lit;
 import net.minecraft.commands.CommandSourceStack;
 
 public class DebugCommands {
-    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(lit("devtest", arg("cmd", StringArgumentType.greedyString(), c -> {
-            System.out.println(SimpleCommandUpgrader.updateSingle(StringArgumentType.getString(c, "cmd")));
-            return 0;
-        })));
-        dispatcher.register(lit("updatecommandjson", c -> {
-            CommandJsonDumpUpgrader.doUpdate();
-            return 0;
-        }));
-        dispatcher.register(lit("updatecommandmeow", c -> {
-            CommandFunctionDumpUpgrader.doUpdate();
-            return 0;
-        }));
-        dispatcher.register(lit("updatecommandsqjson", c -> {
-            CommandSQJsonDumpUpgrader.doUpdate();
-            return 0;
-        }));
-    }
+	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+		dispatcher.register(lit("devtest", arg("cmd", StringArgumentType.greedyString(), c -> {
+			System.out.println(SimpleCommandUpgrader.updateSingle(StringArgumentType.getString(c, "cmd")));
+			return 0;
+		})));
+		dispatcher.register(lit("updatecommandjson", c -> {
+			CommandJsonDumpUpgrader.doUpdate();
+			return 0;
+		}));
+		dispatcher.register(lit("updatecommandmeow", c -> {
+			CommandFunctionDumpUpgrader.doUpdate();
+			return 0;
+		}));
+		dispatcher.register(lit("updatecommandsqjson", c -> {
+			CommandSQJsonDumpUpgrader.doUpdate();
+			return 0;
+		}));
+	}
 }
