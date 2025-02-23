d937dfe19ba69e83ee0d9a60d69795bbcd839443
Floweynt <guywithanaxe42@gmail.com>
2025-02-23 15:27:54 -0600

    Skip inactiveTick if mob is NoAI

---
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/bugfix/MobMixin.java
@@ -0,0 +1,22 @@
+package com.playmonumenta.papermixins.mixin.behavior.bugfix;
+
+import com.playmonumenta.papermixins.ConfigManager;
+import net.minecraft.world.entity.Mob;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Mob.class)
+public abstract class MobMixin {
+	@Shadow
+	public abstract boolean isNoAi();
+
+	@Inject(method = "inactiveTick", at = @At("HEAD"), cancellable = true)
+	private void skipInactiveTick(CallbackInfo ci) {
+		if (isNoAi() && ConfigManager.getConfig().behavior.fixInactiveTickNoAI) {
+			ci.cancel();
+		}
+	}
+}
