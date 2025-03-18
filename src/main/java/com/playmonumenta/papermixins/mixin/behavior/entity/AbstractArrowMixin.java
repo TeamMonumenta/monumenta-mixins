caaee91d9e742c591e58e14366eafa61c55e3e19
U5B <56985400+U5B@users.noreply.github.com>
2025-03-17 18:36:55 -0700

    format

--- src/main/java/com/playmonumenta/papermixins/mixin/behavior/entity/AbstractArrowMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/entity/AbstractArrowMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
-
 import org.bukkit.event.entity.EntityRemoveEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
