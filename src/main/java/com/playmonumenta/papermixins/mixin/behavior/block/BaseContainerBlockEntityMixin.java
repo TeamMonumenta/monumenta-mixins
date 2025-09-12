c32971e0db5007c55dbd1fdc9e3221b2924bb89f
Floweynt <50057682+Floweynt@users.noreply.github.com>
2025-09-12 15:17:24 -0500

    bump version

--- src/main/java/com/playmonumenta/papermixins/mixin/behavior/block/BaseContainerBlockEntityMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/block/BaseContainerBlockEntityMixin.java
@@ -14,22 +14,22 @@ import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(BaseContainerBlockEntity.class)
 public abstract class BaseContainerBlockEntityMixin extends BlockEntity {
-    @Shadow
-    @Nullable
-    public abstract Component getCustomName();
+	@Shadow
+	@Nullable
+	public abstract Component getCustomName();
 
-    public BaseContainerBlockEntityMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
-        super(type, pos, state);
-    }
+	public BaseContainerBlockEntityMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+		super(type, pos, state);
+	}
 
-    @Override
-    public @NotNull CompoundTag getUpdateTag() {
-        final CompoundTag tag = new CompoundTag();
-        Component name = getCustomName();
-        if (name != null) {
-            tag.putString("CustomName", Component.Serializer.toJson(name));
-        }
+	@Override
+	public @NotNull CompoundTag getUpdateTag() {
+		final CompoundTag tag = new CompoundTag();
+		Component name = getCustomName();
+		if (name != null) {
+			tag.putString("CustomName", Component.Serializer.toJson(name));
+		}
 
-        return tag;
-    }
+		return tag;
+	}
 }
