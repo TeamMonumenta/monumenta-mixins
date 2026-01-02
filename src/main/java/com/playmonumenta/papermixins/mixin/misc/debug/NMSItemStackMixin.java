7b6d51776289b5fef72169dad32a0ca2e05cb8e7
LucyChroma <117107593+LucyChroma@users.noreply.github.com>
2026-01-01 20:42:19 -0500

    spotless

--- src/main/java/com/playmonumenta/papermixins/mixin/misc/debug/NMSItemStackMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/misc/debug/NMSItemStackMixin.java
@@ -12,18 +12,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemStack.class)
 public class NMSItemStackMixin {
-    @Shadow @Final private static Logger LOGGER;
+	@Shadow @Final private static Logger LOGGER;
 
-    // TODO: this may cause crashes in some cases
-    @Inject(
-            method = "setItem",
-            at = @At("HEAD"),
-            cancellable = true
-    )
-    private void deprecateSetItem(Item item, CallbackInfo ci) {
-        ci.cancel();
-        LOGGER.error("setItem() has been deprecated and should not be used, try cloning the stack instead.");
-        LOGGER.error("This call has been ignored, since setting items directly interferes with Custom Item API logic");
-        LOGGER.error("Please fix or nag the plugin developer to fix: ", new UnsupportedOperationException());
-    }
+	// TODO: this may cause crashes in some cases
+	@Inject(
+			method = "setItem",
+			at = @At("HEAD"),
+			cancellable = true
+	)
+	private void deprecateSetItem(Item item, CallbackInfo ci) {
+		ci.cancel();
+		LOGGER.error("setItem() has been deprecated and should not be used, try cloning the stack instead.");
+		LOGGER.error("This call has been ignored, since setting items directly interferes with Custom Item API logic");
+		LOGGER.error("Please fix or nag the plugin developer to fix: ", new UnsupportedOperationException());
+	}
 }
