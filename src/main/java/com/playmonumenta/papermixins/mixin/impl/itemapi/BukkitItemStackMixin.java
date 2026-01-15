77f71db855822ed8b544ff2a950456f78c836d71
LucyChroma <117107593+LucyChroma@users.noreply.github.com>
2026-01-15 17:16:46 -0500

    temp commit

--- src/main/java/com/playmonumenta/papermixins/mixin/impl/itemapi/BukkitItemStackMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/impl/itemapi/BukkitItemStackMixin.java
@@ -3,9 +3,11 @@ package com.playmonumenta.papermixins.mixin.impl.itemapi;
 import com.playmonumenta.papermixins.duck.ItemStackAccess;
 import com.playmonumenta.papermixins.itemindex.MonumentaInjectedItemData;
 import org.bukkit.inventory.ItemStack;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
+@Debug(export = true)
 @Mixin(ItemStack.class)
 public class BukkitItemStackMixin implements ItemStackAccess {
 	@Unique
@@ -13,11 +15,17 @@ public class BukkitItemStackMixin implements ItemStackAccess {
 
 	@Unique
 	public void monumenta$setInjectedData(MonumentaInjectedItemData data) {
-		injectedData = data;
+		System.out.println("saved, data is " + (data == null ? "" : "not ") + "null");
+		this.injectedData = data;
+		System.out.println("data is now " + (injectedData == null ? "" : "not ") + "null");
+		if (injectedData != null) {
+			System.out.println(injectedData);
+		}
 	}
 
 	@Unique
 	public MonumentaInjectedItemData monumenta$getInjectedData() {
+		System.out.println("loading, data is " + (injectedData == null ? "" : "not ") + "null");
 		return injectedData;
 	}
 }
