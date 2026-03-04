461fd03537fb6f5a1543e1ef5fcc175528719dba
LucyChroma <117107593+LucyChroma@users.noreply.github.com>
2026-03-04 02:33:22 -0500

    cache sort instead of re-sorting every time

--- src/main/java/com/playmonumenta/papermixins/mixin/behavior/advancement/AdvancementNodeMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/advancement/AdvancementNodeMixin.java
@@ -4,20 +4,33 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import net.minecraft.advancements.AdvancementNode;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AdvancementNode.class)
 public class AdvancementNodeMixin {
     @Shadow @Final private Set<AdvancementNode> children;
 
+    @Unique @Nullable
+    List<AdvancementNode> monumenta$cachedChildren;
+
+    @Inject(method = "addChild(Lnet/minecraft/advancements/AdvancementNode;)V", at = @At("HEAD"))
+    private void addChildInject(AdvancementNode ignored, CallbackInfo ci) {
+        monumenta$cachedChildren = null; // invalidate cached sort
+    }
+
     @Inject(method = "children()Ljava/lang/Iterable;", at=@At("HEAD"), cancellable = true)
     private void getChildrenInject(CallbackInfoReturnable<Iterable<AdvancementNode>> cir) {
-        List<AdvancementNode> l = children.stream().sorted(Comparator.comparing(o -> o.holder().id())).toList();
-        cir.setReturnValue(l);
+        if (monumenta$cachedChildren == null) {
+            monumenta$cachedChildren = children.stream().sorted(Comparator.comparing(o -> o.holder().id())).toList();
+        }
+        cir.setReturnValue(monumenta$cachedChildren);
     }
 }
