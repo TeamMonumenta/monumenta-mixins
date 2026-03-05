a996a60f2ee41eb6aec9c1c9fd98c5b4ce94e052
LucyChroma <117107593+LucyChroma@users.noreply.github.com>
2026-03-04 19:04:09 -0500

    advancement tabs as well

---
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/advancement/AdvancementNodeCompare.java
@@ -0,0 +1,16 @@
+package com.playmonumenta.papermixins.mixin.behavior.advancement;
+
+import com.playmonumenta.papermixins.duck.AdvancementAccess;
+import com.playmonumenta.papermixins.util.Util;
+import net.minecraft.advancements.AdvancementNode;
+
+public final class AdvancementNodeCompare {
+    public static int compareAdvancementNodes(AdvancementNode o1, AdvancementNode o2) {
+        // This used to be a one-line "Comparator.compares.thenComparing". You don't want to know what it looked like.
+        // Also this can't just be a static method in the mixin class because of mixin rules
+        AdvancementAccess access1 = Util.c(o1.holder().value());
+        AdvancementAccess access2 = Util.c(o2.holder().value());
+        int priorityDifference = access1.monumenta$getPriority() - access2.monumenta$getPriority();
+        return priorityDifference != 0 ? priorityDifference : o1.holder().id().compareTo(o2.holder().id());
+    }
+}
--- src/main/java/com/playmonumenta/papermixins/mixin/optimize/AdvancementTreeMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/optimize/AdvancementTreeMixin.java
@@ -1,6 +1,10 @@
 package com.playmonumenta.papermixins.mixin.optimize;
 
+import com.playmonumenta.papermixins.duck.AdvancementAccess;
 import com.playmonumenta.papermixins.duck.AdvancementTreeAccess;
+import com.playmonumenta.papermixins.mixin.behavior.advancement.AdvancementNodeCompare;
+import com.playmonumenta.papermixins.util.Util;
+import it.unimi.dsi.fastutil.objects.ObjectLinkedOpenHashSet;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -8,6 +12,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.AdvancementNode;
@@ -21,6 +26,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * Improve diagnostics for Advancement parsing
@@ -37,6 +43,8 @@ public abstract class AdvancementTreeMixin implements AdvancementTreeAccess {
 	@Shadow
 	@Final
 	private Map<ResourceLocation, AdvancementNode> nodes;
+
+	@Shadow @Final private Set<AdvancementNode> roots;
 
 	// TODO: we can technically optimize this faster by e bitsets and converting ResourceLocation to index
 	@Unique
@@ -154,5 +162,11 @@ public abstract class AdvancementTreeMixin implements AdvancementTreeAccess {
 	@Override
 	public void monumenta$addAllFast(Map<ResourceLocation, AdvancementHolder> advancements) {
 		monumenta$addAllFastImpl(new ArrayList<>(advancements.values()), advancements);
+	}
+
+	@Inject(method = "roots()Ljava/lang/Iterable;", at = @At("HEAD"), cancellable = true)
+	public void rootsInject(CallbackInfoReturnable<Iterable<AdvancementNode>> cir) {
+		List<AdvancementNode> sorted = roots.stream().sorted(AdvancementNodeCompare::compareAdvancementNodes).toList();
+		cir.setReturnValue(sorted);
 	}
 }
