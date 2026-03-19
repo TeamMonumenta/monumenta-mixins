01c82112523f21cf9fc3cc3d1b6ecc9e7a27bc57
LucyChroma <117107593+LucyChroma@users.noreply.github.com>
2026-03-19 16:24:15 -0400

    parameters

--- src/main/java/com/playmonumenta/papermixins/duck/AdvancementAccess.java
+++ src/main/java/com/playmonumenta/papermixins/duck/AdvancementAccess.java
@@ -4,4 +4,16 @@ public interface AdvancementAccess {
 	int monumenta$getPriority();
 
 	void monumenta$setPriority(int priority);
+	
+	String monumenta$getTreePositionType();
+	
+	void monumenta$setTreePositionType(String monumenta$treePositionType);
+	
+	int monumenta$getTreePositionX();
+	
+	void monumenta$setTreePositionX(int monumenta$treePositionX);
+	
+	int monumenta$getTreePositionY();
+	
+	void monumenta$setTreePositionY(int monumenta$treePositionY);
 }
--- src/main/java/com/playmonumenta/papermixins/mixin/behavior/advancement/AdvancementMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/advancement/AdvancementMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.advancements.AdvancementRequirements;
 import net.minecraft.advancements.AdvancementRewards;
 import net.minecraft.advancements.Criterion;
 import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.ExtraCodecs;
 import org.spongepowered.asm.mixin.Final;
@@ -24,23 +25,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Advancement.class)
 public class AdvancementMixin implements AdvancementAccess {
-	@Unique
-	private int monumenta$priority = 0;
+	@Unique private int monumenta$priority = 0;
+	@Unique private String monumenta$treePositionType = "relative";
+	@Unique private int monumenta$treePositionX = 0;
+	@Unique private int monumenta$treePositionY = 0;
 
 	@Mutable
 	@Shadow @Final public static Codec<Advancement> CODEC;
 
 	@Shadow @Final private static Codec<Map<String, Criterion<?>>> CRITERIA_CODEC;
 
-	@Unique
-	public int monumenta$getPriority() {
-		return monumenta$priority;
-	}
-
-	@Unique
-	public void monumenta$setPriority(int priority) {
-		monumenta$priority = priority;
-	}
+	@Shadow
+	@Final
+	@SuppressWarnings("OptionalUsedAsFieldOrParameterType") // it's shadowed
+	private Optional<DisplayInfo> display;
 
 	@Inject(method = "<clinit>", at = @At("TAIL"))
 	private static void modifyCodec(CallbackInfo ci) {
@@ -54,12 +52,18 @@ public class AdvancementMixin implements AdvancementAccess {
 										ExtraCodecs.strictOptionalField(AdvancementRequirements.CODEC, "requirements")
 												.forGetter(a -> Optional.of(a.requirements())),
 										ExtraCodecs.strictOptionalField(Codec.BOOL, "sends_telemetry_event", false).forGetter(Advancement::sendsTelemetryEvent),
-										ExtraCodecs.strictOptionalField(Codec.INT, "priority", 0).forGetter(a -> Util.<AdvancementAccess>c(a).monumenta$getPriority())
+										ExtraCodecs.strictOptionalField(Codec.INT, "priority", 0).forGetter(a -> Util.<AdvancementAccess>c(a).monumenta$getPriority()),
+										ExtraCodecs.strictOptionalField(Codec.STRING, "positionType", "relative").forGetter(a -> Util.<AdvancementAccess>c(a).monumenta$getTreePositionType()),
+										ExtraCodecs.strictOptionalField(Codec.INT, "positionX", 0).forGetter(a -> Util.<AdvancementAccess>c(a).monumenta$getTreePositionX()),
+										ExtraCodecs.strictOptionalField(Codec.INT, "positionY", 0).forGetter(a -> Util.<AdvancementAccess>c(a).monumenta$getTreePositionY())
 								)
-								.apply(instance, (parent, display, rewards, criteria, requirements, sendsTelemetryEvent, priority) -> { // priority
+								.apply(instance, (parent, display, rewards, criteria, requirements, sendsTelemetryEvent, priority, positionType, positionX, positionY) -> {
 									AdvancementRequirements advancementRequirements = requirements.orElseGet(() -> AdvancementRequirements.allOf(criteria.keySet()));
 									Advancement adv = new Advancement(parent, display, rewards, criteria, advancementRequirements, sendsTelemetryEvent);
 									Util.<AdvancementAccess>c(adv).monumenta$setPriority(priority);
+									Util.<AdvancementAccess>c(adv).monumenta$setTreePositionType(positionType);
+									Util.<AdvancementAccess>c(adv).monumenta$setTreePositionX(positionX);
+									Util.<AdvancementAccess>c(adv).monumenta$setTreePositionY(positionY);
 									return adv;
 								})
 				),
@@ -69,4 +73,50 @@ public class AdvancementMixin implements AdvancementAccess {
 
 	// Advancement.read(FriendlyByteBuf) also creates advancements, but we don't need to modify it
 	// because it's intended for clients parsing advancements from registry packets
+
+	@Unique
+	public int monumenta$getPriority() {
+		return monumenta$priority;
+	}
+
+	@Unique
+	public void monumenta$setPriority(int priority) {
+		monumenta$priority = priority;
+	}
+
+	@Unique
+	public String monumenta$getTreePositionType() {
+		return monumenta$treePositionType;
+	}
+
+	@Unique
+	public void monumenta$setTreePositionType(String positionType) {
+		if (!positionType.equals("absolute") && !positionType.equals("relative")) {
+			Component title = this.display.orElseThrow(() -> new IllegalArgumentException("advancement missing name!")).getTitle();
+			String title1 = String.join("", title.toFlatList().stream().map(Component::getString).toList());
+			throw new IllegalArgumentException("positionType must be either absolute or relative, got \"%s\" for advancement \"%s\""
+					.formatted(positionType, title1));
+		}
+		this.monumenta$treePositionType = positionType;
+	}
+
+	@Unique
+	public int monumenta$getTreePositionX() {
+		return monumenta$treePositionX;
+	}
+
+	@Unique
+	public void monumenta$setTreePositionX(int monumenta$treePositionX) {
+		this.monumenta$treePositionX = monumenta$treePositionX;
+	}
+
+	@Unique
+	public int monumenta$getTreePositionY() {
+		return monumenta$treePositionY;
+	}
+
+	@Unique
+	public void monumenta$setTreePositionY(int monumenta$treePositionY) {
+		this.monumenta$treePositionY = monumenta$treePositionY;
+	}
 }
