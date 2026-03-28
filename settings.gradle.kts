856da10f1c294bd7394d78a90f215cd3aecbc2c3
Byron Marohn <Combustible@live.com>
2026-03-28 10:03:41 -0700

    Update build to use the common monumenta gradle plugin
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+	repositories {
+		mavenLocal()
+		gradlePluginPortal()
+		maven("https://maven.playmonumenta.com/releases/")
+	}
+}
+
 rootProject.name = "monumenta-mixins"
 
 include("plugin-api")
