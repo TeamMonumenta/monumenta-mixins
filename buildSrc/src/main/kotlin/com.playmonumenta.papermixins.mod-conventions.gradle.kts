fe847212f548d43a8de4ba8afefb82fa584e7c7f
Byron Marohn <Combustible@live.com>
2026-04-01 14:30:42 -0700

    Restore afterEvaluate gradle configuration
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- buildSrc/src/main/kotlin/com.playmonumenta.papermixins.mod-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.playmonumenta.papermixins.mod-conventions.gradle.kts
@@ -17,8 +17,10 @@ configurations.getByName("implementation").extendsFrom(include)
 
 repositories {
     mavenCentral()
+    maven("https://maven.playmonumenta.com/releases")
     maven("https://maven.fabricmc.net/")
     maven("https://repo.spongepowered.org/maven/")
+    maven("https://repo.codemc.io/repository/maven-public/")
 }
 
 configurations.getByName("runtimeClasspath").extendsFrom(configurations.getByName("mojangMappedServerRuntime"))
@@ -45,4 +47,60 @@ tasks {
     reobfJar {
         remapperArgs = TinyRemapper.createArgsList() + "--mixin"
     }
+}
+
+afterEvaluate {
+    val mixinJar = configurations.runtimeClasspath.get()
+        .resolvedConfiguration
+        .resolvedArtifacts
+        .filter { artifact -> artifact.moduleVersion.id.module.toString().equals("net.fabricmc:sponge-mixin") }
+        .also { assert(it.size == 1) }
+        .first()
+        .file
+        .absolutePath
+
+    val toolchains = mapOf(
+        "Jbr17" to javaToolchains.launcherFor {
+            languageVersion = JavaLanguageVersion.of(17)
+            vendor = JvmVendorSpec.JETBRAINS
+        },
+        "Jbr21" to javaToolchains.launcherFor {
+            languageVersion = JavaLanguageVersion.of(21)
+            vendor = JvmVendorSpec.JETBRAINS
+        },
+        "Jdk17" to javaToolchains.launcherFor {
+            languageVersion = JavaLanguageVersion.of(17)
+        },
+        "Jdk21" to javaToolchains.launcherFor {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    )
+
+    fun configureService(postfix: String, toolchain: Provider<JavaLauncher>?) {
+        tasks.register<JavaExec>("runServer$postfix") {
+            workingDir = project.projectDir.resolve("run")
+            classpath = sourceSets.main.get().runtimeClasspath
+            mainClass = "com.floweytf.fabricpaperloader.Main"
+            group = "runs"
+            systemProperties = mapOf(
+                "fabric.development" to true,
+                "mixin.debug.export" to true,
+                "fabric.classPathGroups" to sourceSets.main.get().output.files.joinToString(File.pathSeparator)
+            )
+            standardInput = System.`in`
+            standardOutput = System.out
+            errorOutput = System.err
+            toolchain?.let { javaLauncher = it }
+            jvmArgs(
+                "-XX:+AllowEnhancedClassRedefinition",
+                "-XX:+IgnoreUnrecognizedVMOptions",
+                "-javaagent:$mixinJar",
+            )
+            args("-nogui")
+
+        }
+    }
+
+    toolchains.forEach(::configureService)
+    configureService("", null)
 }
