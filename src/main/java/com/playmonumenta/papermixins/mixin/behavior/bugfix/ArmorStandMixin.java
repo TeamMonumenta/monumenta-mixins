19926d43cef52a8a3b5d749d2c30859134df1fa0
jade <infernoblaze326@gmail.com>
2026-04-05 11:04:51 +0800

    fix armor stands not returning true for isDead() during its Death Event

---
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/bugfix/ArmorStandMixin.java
@@ -0,0 +1,22 @@
+package com.playmonumenta.papermixins.mixin.behavior.bugfix;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ArmorStand.class)
+public abstract class ArmorStandMixin extends LivingEntity {
+	protected ArmorStandMixin(EntityType<? extends LivingEntity> type, Level world) {
+		super(type, world);
+	}
+
+	@Inject(method = "kill(Z)V", at = @At("HEAD"))
+	private void setHealthZero(boolean callEvent, CallbackInfo ci) {
+		this.setHealth(0);
+	}
+}
