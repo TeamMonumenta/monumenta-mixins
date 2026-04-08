ed8e57ad1f5bb324187434f8855bb5f9d23c3258
jade <infernoblaze326@gmail.com>
2026-04-08 22:56:52 +0800

    address review

--- src/main/java/com/playmonumenta/papermixins/mixin/behavior/bugfix/ArmorStandMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/bugfix/ArmorStandMixin.java
@@ -1,13 +1,16 @@
 package com.playmonumenta.papermixins.mixin.behavior.bugfix;
 
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.level.Level;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ArmorStand.class)
 public abstract class ArmorStandMixin extends LivingEntity {
@@ -16,7 +19,12 @@ public abstract class ArmorStandMixin extends LivingEntity {
 	}
 
 	@Inject(method = "kill(Z)V", at = @At("HEAD"))
-	private void setHealthZero(boolean callEvent, CallbackInfo ci) {
+	private void onKill(boolean callEvent, CallbackInfo ci) {
+		this.setHealth(0);
+	}
+
+	@Inject(method = "brokenByAnything", at = @At("HEAD"))
+	private void onBroken(DamageSource damageSource, CallbackInfoReturnable<EntityDeathEvent> cir) {
 		this.setHealth(0);
 	}
 }
