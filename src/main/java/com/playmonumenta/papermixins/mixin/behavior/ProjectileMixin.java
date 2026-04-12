207b3b32368a1f8a6b5ae88b6575dd711f0d275c
jade <infernoblaze326@gmail.com>
2026-04-12 19:26:18 +0800

    add dont hit player functionality

--- src/main/java/com/playmonumenta/papermixins/mixin/behavior/ProjectileMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/ProjectileMixin.java
@@ -1,4 +1,4 @@
-package com.playmonumenta.papermixins.mixin.behavior.bugfix;
+package com.playmonumenta.papermixins.mixin.behavior;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
@@ -7,10 +7,17 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+
+import javax.annotation.Nullable;
 
 @Mixin(Projectile.class)
 public abstract class ProjectileMixin extends Entity {
+	@Shadow
+	@Nullable
+	public abstract Entity getOwner();
+
 	public ProjectileMixin(EntityType<?> type, Level world) {
 		super(type, world);
 	}
@@ -22,7 +29,9 @@ public abstract class ProjectileMixin extends Entity {
 			target = "Lnet/minecraft/world/entity/Entity;canBeHitByProjectile()Z"
 		)
 	)
-	public boolean ignoreProjectilePassthrough(boolean original, @Local(argsOnly = true) Entity entity) {
-		return original && !entity.getTags().contains("projectile_passthrough");
+	public boolean canHitEntity(boolean original, @Local(argsOnly = true) Entity entity) {
+		Entity owner = getOwner();
+		boolean playerHittingPlayer = owner == null || owner.getType() != EntityType.PLAYER || entity.getType() != EntityType.PLAYER;
+		return original && !entity.getTags().contains("projectile_passthrough") && !playerHittingPlayer;
 	}
 }
