d42b9272fe3e3d712379b953b7d68133428b6c40
jade <infernoblaze326@gmail.com>
2026-04-12 19:35:59 +0800

    reviewdog 2

--- src/main/java/com/playmonumenta/papermixins/mixin/behavior/ProjectileMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/ProjectileMixin.java
@@ -3,11 +3,11 @@ package com.playmonumenta.papermixins.mixin.behavior;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.playmonumenta.papermixins.ConfigManager;
-import javax.annotation.Nullable;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,6 +30,7 @@ public abstract class ProjectileMixin extends Entity {
 		)
 	)
 	public boolean canHitEntity(boolean original, @Local(argsOnly = true) Entity entity) {
+		@Nullable
 		Entity owner = getOwner();
 		boolean playerHittingPlayer = owner != null && owner.getType() == EntityType.PLAYER && entity.getType() == EntityType.PLAYER;
 		return original &&
