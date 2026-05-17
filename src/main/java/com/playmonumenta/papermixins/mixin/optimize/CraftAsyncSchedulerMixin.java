9452aec9a936d1c9fe319f831066a198a4610fda
Floweynt <50057682+Floweynt@users.noreply.github.com>
2026-05-16 21:07:28 -0500

    tabs

--- src/main/java/com/playmonumenta/papermixins/mixin/optimize/CraftAsyncSchedulerMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/optimize/CraftAsyncSchedulerMixin.java
@@ -16,43 +16,43 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(CraftAsyncScheduler.class)
 public class CraftAsyncSchedulerMixin {
-    @Unique
-    private final ExecutorService monumenta$threadPool = Executors.newThreadPerTaskExecutor(
-        Thread.ofVirtual()
-            .name("Craft Scheduler VThread - ", 0)
-            .uncaughtExceptionHandler(new DefaultUncaughtExceptionHandlerWithName(MinecraftServer.LOGGER))
-            .factory()
-    );
+	@Unique
+	private final ExecutorService monumenta$threadPool = Executors.newThreadPerTaskExecutor(
+		Thread.ofVirtual()
+			.name("Craft Scheduler VThread - ", 0)
+			.uncaughtExceptionHandler(new DefaultUncaughtExceptionHandlerWithName(MinecraftServer.LOGGER))
+			.factory()
+	);
 
-    @ModifyExpressionValue(
-        method = "<init>",
-        at = @At(value = "NEW", target = "(IIJLjava/util/concurrent/TimeUnit;Ljava/util/concurrent/BlockingQueue;" +
-            "Ljava/util/concurrent/ThreadFactory;)Ljava/util/concurrent/ThreadPoolExecutor;")
-    )
-    private ThreadPoolExecutor replaceExecutor(ThreadPoolExecutor original) {
-        return null;
-    }
+	@ModifyExpressionValue(
+		method = "<init>",
+		at = @At(value = "NEW", target = "(IIJLjava/util/concurrent/TimeUnit;Ljava/util/concurrent/BlockingQueue;" +
+			"Ljava/util/concurrent/ThreadFactory;)Ljava/util/concurrent/ThreadPoolExecutor;")
+	)
+	private ThreadPoolExecutor replaceExecutor(ThreadPoolExecutor original) {
+		return null;
+	}
 
-    @WrapOperation(
-        method = "<init>",
-        at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ThreadPoolExecutor;allowCoreThreadTimeOut(Z)V")
-    )
-    private void noop0(ThreadPoolExecutor instance, boolean value, Operation<Void> original) {
-    }
+	@WrapOperation(
+		method = "<init>",
+		at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ThreadPoolExecutor;allowCoreThreadTimeOut(Z)V")
+	)
+	private void noop0(ThreadPoolExecutor instance, boolean value, Operation<Void> original) {
+	}
 
-    @WrapOperation(
-        method = "<init>",
-        at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ThreadPoolExecutor;prestartAllCoreThreads()I")
-    )
-    private int noop1(ThreadPoolExecutor instance, Operation<Integer> original) {
-        return 0;
-    }
+	@WrapOperation(
+		method = "<init>",
+		at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ThreadPoolExecutor;prestartAllCoreThreads()I")
+	)
+	private int noop1(ThreadPoolExecutor instance, Operation<Integer> original) {
+		return 0;
+	}
 
-    @Redirect(
-        method = "executeTask",
-        at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ThreadPoolExecutor;execute(Ljava/lang/Runnable;)V")
-    )
-    private void scheduleOnVirtualThreads(ThreadPoolExecutor instance, Runnable command) {
-        monumenta$threadPool.execute(command);
-    }
+	@Redirect(
+		method = "executeTask",
+		at = @At(value = "INVOKE", target = "Ljava/util/concurrent/ThreadPoolExecutor;execute(Ljava/lang/Runnable;)V")
+	)
+	private void scheduleOnVirtualThreads(ThreadPoolExecutor instance, Runnable command) {
+		monumenta$threadPool.execute(command);
+	}
 }
