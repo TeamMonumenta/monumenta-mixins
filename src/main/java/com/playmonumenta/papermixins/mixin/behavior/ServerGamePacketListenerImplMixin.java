6243a8e18a48c81f3380f36070f61eb131dac3a0
jade <infernoblaze326@gmail.com>
2026-06-02 12:43:47 +0800

    c

--- src/main/java/com/playmonumenta/papermixins/mixin/behavior/ServerGamePacketListenerImplMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/ServerGamePacketListenerImplMixin.java
@@ -2,12 +2,9 @@ package com.playmonumenta.papermixins.mixin.behavior;
 
 import com.playmonumenta.papermixins.ConfigManager;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
-import net.minecraft.world.inventory.InventoryMenu;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * @author Flowey
