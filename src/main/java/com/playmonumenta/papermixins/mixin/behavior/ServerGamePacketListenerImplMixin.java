9e2528214ae43f0a6523609f730b584953566f57
jade <infernoblaze326@gmail.com>
2026-06-02 09:56:29 +0800

    a

--- src/main/java/com/playmonumenta/papermixins/mixin/behavior/ServerGamePacketListenerImplMixin.java
+++ src/main/java/com/playmonumenta/papermixins/mixin/behavior/ServerGamePacketListenerImplMixin.java
@@ -2,9 +2,12 @@ package com.playmonumenta.papermixins.mixin.behavior;
 
 import com.playmonumenta.papermixins.ConfigManager;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.inventory.InventoryMenu;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * @author Flowey
@@ -22,5 +25,16 @@ public class ServerGamePacketListenerImplMixin {
 	)
 	private int changeFlyingTickTime(int constant) {
 		return ConfigManager.getConfig().flyingTime;
+	}
+
+	@Redirect(
+		method = "handleContainerClick",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/world/inventory/InventoryMenu;incrementStateId()I"
+		)
+	)
+	public int doNotIncrement(InventoryMenu instance) {
+		return instance.getStateId();
 	}
 }
