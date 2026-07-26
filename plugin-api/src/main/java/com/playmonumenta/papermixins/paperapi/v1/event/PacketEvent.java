a2fbd2740d8f9decabc7b44bb55758f4ae7bf139
jade <infernoblaze326@gmail.com>
2026-07-26 19:13:33 +0800

    aaaaaaaaaaa

---
+++ plugin-api/src/main/java/com/playmonumenta/papermixins/paperapi/v1/event/PacketEvent.java
@@ -0,0 +1,73 @@
+package com.playmonumenta.papermixins.paperapi.v1.event;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class PacketEvent extends PlayerEvent implements Cancellable {
+	// throw this in version adapter, teehee!
+	private Object packet;
+	private boolean cancelled = false;
+	private boolean changed = false;
+	@Nullable
+	private final List<Object> allBundledPackets;
+
+	public PacketEvent(Player player, Object packet, @Nullable ArrayList<Object> allBundledPackets) {
+		super(player);
+		this.packet = packet;
+		this.allBundledPackets = allBundledPackets;
+	}
+
+	public Object getPacket() {
+		return packet;
+	}
+
+	public void setPacket(Object newPacket) {
+		this.packet = newPacket;
+		changed = true;
+	}
+
+	public boolean packetChanged() {
+		return changed;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return this.cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancel) {
+		this.cancelled = cancel;
+	}
+
+	public boolean isBundlePacket() {
+		return allBundledPackets != null;
+	}
+
+	public void modifyBundlePackets(Consumer<List<Object>> packetHandler) {
+		if (allBundledPackets != null) {
+			packetHandler.accept(allBundledPackets);
+		}
+	}
+
+	@NotNull
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	private static final HandlerList handlers = new HandlerList();
+
+	@NotNull
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
---
+++ src/main/java/com/playmonumenta/papermixins/mixin/api/ConnectionMixin.java
@@ -0,0 +1,75 @@
+package com.playmonumenta.papermixins.mixin.api;
+
+import com.playmonumenta.papermixins.paperapi.v1.event.PacketEvent;
+import io.papermc.paper.util.MCUtil;
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBundlePacket;
+import net.minecraft.server.level.ServerPlayer;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.ArrayList;
+
+@Mixin(Connection.class)
+public abstract class ConnectionMixin {
+	@Shadow
+	public abstract ServerPlayer getPlayer();
+
+	@ModifyVariable(
+		method = "send(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketSendListener;Z)V",
+		at = @At("HEAD"),
+		argsOnly = true
+	)
+	@SuppressWarnings("unchecked")
+	public Packet<?> onSend(Packet<?> original) {
+		@Nullable
+		ServerPlayer player = getPlayer();
+		if (player == null) {
+			return original;
+		}
+		if (!MCUtil.isMainThread()) {
+			return original;
+		}
+		if (original instanceof ClientboundBundlePacket bundlePacket) {
+			Iterable<Packet<ClientGamePacketListener>> packets = bundlePacket.subPackets();
+			ArrayList<Object> subPacketList = new ArrayList<>();
+			for (Packet<ClientGamePacketListener> p : packets) {
+				subPacketList.add(p);
+			}
+			ArrayList<Packet<ClientGamePacketListener>> newSubPacketList = new ArrayList<>(subPacketList.size());
+			boolean modified = false;
+			for (Packet<ClientGamePacketListener> originalPacket : packets) {
+				PacketEvent event = new PacketEvent(player.getBukkitEntity(), originalPacket, subPacketList);
+				event.callEvent();
+				if (event.isCancelled()) {
+					continue;
+				}
+				if (event.packetChanged() && event.getPacket() instanceof Packet<?> newPacket) {
+					modified = true;
+					// oh well, sorry type safety!
+					newSubPacketList.add((Packet<ClientGamePacketListener>) newPacket);
+					continue;
+				}
+				newSubPacketList.add(originalPacket);
+			}
+			if (modified) {
+				return new ClientboundBundlePacket(newSubPacketList);
+			}
+		} else {
+			PacketEvent event = new PacketEvent(player.getBukkitEntity(), original, null);
+			event.callEvent();
+			if (event.isCancelled()) {
+				return original;
+			}
+			if (event.packetChanged() && event.getPacket() instanceof Packet<?> newPacket) {
+				return newPacket;
+			}
+		}
+		return original;
+	}
+}
